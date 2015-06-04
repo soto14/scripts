5043246cb082d67622b40cc3b99828c22e848115
Eric Soto <eric.gabriel.soto@gmail.com>
2015-06-04 16:14:19 -0400

    Fixed Bug
    Updated script to use the output directory passed in to the command line
    and not process directories in the input directory

--- python/soapMessageCreator.py
+++ python/soapMessageCreator.py
@@ -22,18 +22,19 @@ if not(os.path.isdir(soapDir)):
 
 print "Processing: " + inputDir
 for file in os.listdir(inputDir):
-    filePath = inputDir + os.sep + file
-    soapFilePath = "soap" + os.sep + "soap_" + file
-    print "Converting: " + filePath
-    #open the pd file for reading
-    xmlFile = open(filePath, "r")
-    #no need to check for soapFile existance, using "w" will overwrite existing file content.
-    soapFile = open(soapFilePath, "w")
-    soapFile.write("<?xml version=\"1.0\"?>\n<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n<soapenv:Header/>\n<soapenv:Body>\n")
-    for line in xmlFile:
-        #do not write the root xml element from the file being read
-        if line.find( "<?xml version=\"1.0\"?>" ) == -1:
-            soapFile.write(line)
-    soapFile.write("</soapenv:Body>\n</soapenv:Envelope>\n")
-    soapFile.close()
-    xmlFile.close()
+    if not(os.path.isdir(file)):
+        filePath = inputDir + os.sep + file
+        soapFilePath = soapDir + os.sep + "soap_" + file
+        print "Converting: " + filePath
+        #open the pd file for reading
+        xmlFile = open(filePath, "r")
+        #no need to check for soapFile existance, using "w" will overwrite existing file content.
+        soapFile = open(soapFilePath, "w")
+        soapFile.write("<?xml version=\"1.0\"?>\n<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n<soapenv:Header/>\n<soapenv:Body>\n")
+        for line in xmlFile:
+            #do not write the root xml element from the file being read
+            if line.find( "<?xml version=\"1.0\"?>" ) == -1:
+                soapFile.write(line)
+        soapFile.write("</soapenv:Body>\n</soapenv:Envelope>\n")
+        soapFile.close()
+        xmlFile.close()
